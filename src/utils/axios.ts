d23da7a6c92fc03874d3b42127f3ae843829ff36
Prakash <Prakashkumarggh@gmail.com>
2025-10-05 20:18:25 +0530

    refactor(axios): update default API base URL to mock server

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:8000";
+const API_BASE = import.meta.env.VITE_API_BASE || "https://my-json-server.typicode.com/Psr-mathur/student-learning-assistant";
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE,
